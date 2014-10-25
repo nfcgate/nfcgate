5497fbef7f221005a23f1930a5a1f02a9202046c
Max Maass <max@velcommuta.de>
2014-10-25 19:18:48 +0200

    Changed the interface a little.

--- app/src/main/java/tud/seemuh/nfcgate/util/NFCTagReader.java
+++ app/src/main/java/tud/seemuh/nfcgate/util/NFCTagReader.java
@@ -1,19 +1,14 @@
 package tud.seemuh.nfcgate.util;
 
 import android.nfc.Tag;
+import java.util.Hashtable;
+
 
 /**
  * Interface to all NFCTagReader-Classes.
  * Created by Max on 25.10.14.
  */
 public interface NFCTagReader {
-    /**
-     * Read an NFC Tag, returning the Tag object.
-     *
-     * @return Tag object of the read NFC Tag
-     */
-    public Tag readTag();
-
     /**
      * Send a raw command to the NFC chip, receiving the answer as a byte[]
      *
@@ -24,4 +19,16 @@ public interface NFCTagReader {
      *       automatically calculated. "
      */
     public byte[] sendCmd(byte[] command);
+
+    /**
+     * Returns a HashTable containing meta-bytes. The exact bytes are protocol-specific.
+     * An exact list of which bytes will be returned will be added as soon as the functionality
+     * is finished.
+     *
+     * @return A Hashtable<String, byte[]> containing a mapping between the meta-byte names
+     *     and their values.
+     *
+     * TODO: Add list of provided bytes.
+     */
+    public Hashtable<String, byte[]> getMetaBytes();
 }
