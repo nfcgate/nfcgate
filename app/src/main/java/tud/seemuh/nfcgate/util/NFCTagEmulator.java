0a787e56aaebb5271cbcdcb9a26efdc6780d8595
Max Maass <max@velcommuta.de>
2014-10-26 21:10:39 +0100

    Updated interfaces. Will probably switch to feature branch now.

--- app/src/main/java/tud/seemuh/nfcgate/util/NFCTagEmulator.java
+++ app/src/main/java/tud/seemuh/nfcgate/util/NFCTagEmulator.java
@@ -6,6 +6,20 @@ package tud.seemuh.nfcgate.util;
  * Created by Max on 25.10.14.
  */
 public interface NFCTagEmulator {
+    // These constants are a proposal to the developer of this part of the app
+    // If you find that they do not fit your needs, feel free to change them.
+    // Just notify everyone else who may be using them :)
+    // (It may be that individual protocols have to be emulated using different classes,
+    // this is why these constants are defined to distinguish between them)
+    public static final int EMULATOR_ISODEP            = 0;
+    public static final int EMULATOR_MIFARE_CLASSIC    = 1;
+    public static final int EMULATOR_MIFARE_ULTRALIGHT = 2;
+    public static final int EMULATOR_NFC_A             = 3;
+    public static final int EMULATOR_NFC_B             = 4;
+    public static final int EMULATOR_NFC_BARCODE       = 5;
+    public static final int EMULATOR_NFC_F             = 6;
+    public static final int EMULATOR_NFC_V             = 7;
+    
     /**
      * Send a command or response to the card reader.
      * @param command The byte[]-representation of the command to be sent
@@ -14,4 +28,12 @@ public interface NFCTagEmulator {
      * TODO: Note to dev: Probably a case for "sendResponseApdu()" from HostAdpuService
      */
     public byte[] sendCmd(byte[] command);
+
+    /**
+     * Returns the integer representation of the protocol the used implementation is speaking.
+     * This will be one of the EMULATOR_* constants defined by the util.NFCTagReader interface.
+     *
+     * @return integer representation of the underlying NFC tag reader protocol
+     */
+    public int getProtocol();
 }
--- app/src/main/java/tud/seemuh/nfcgate/util/NFCTagReader.java
+++ app/src/main/java/tud/seemuh/nfcgate/util/NFCTagReader.java
@@ -9,6 +9,15 @@ import java.util.Hashtable;
  * Created by Max on 25.10.14.
  */
 public interface NFCTagReader {
+    public static final int READER_ISODEP            = 0;
+    public static final int READER_MIFARE_CLASSIC    = 1;
+    public static final int READER_MIFARE_ULTRALIGHT = 2;
+    public static final int READER_NFC_A             = 3;
+    public static final int READER_NFC_B             = 4;
+    public static final int READER_NFC_BARCODE       = 5;
+    public static final int READER_NFC_F             = 6;
+    public static final int READER_NFC_V             = 7;
+
     /**
      * Send a raw command to the NFC chip, receiving the answer as a byte[]
      *
@@ -31,4 +40,12 @@ public interface NFCTagReader {
      * TODO: Add list of provided bytes.
      */
     public Hashtable<String, byte[]> getMetaBytes();
+
+    /**
+     * Returns the integer representation of the protocol the used implementation is speaking.
+     * This will be one of the READER_* constants defined by the util.NFCTagReader interface.
+     *
+     * @return integer representation of the underlying NFC tag reader protocol
+     */
+    public int getProtocol();
 }
