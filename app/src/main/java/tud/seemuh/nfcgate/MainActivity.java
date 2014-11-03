eaad2e1d57605f3dc35b902ddb0171d8cfb85f9f
Daniel Wegemer <daniel@wegemer.com>
2014-11-03 21:44:50 +0100

    Implemented BroadcastReceiver

--- app/src/main/java/tud/seemuh/nfcgate/MainActivity.java
+++ app/src/main/java/tud/seemuh/nfcgate/MainActivity.java
@@ -2,8 +2,11 @@ package tud.seemuh.nfcgate;
 
 import android.app.Activity;
 import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.net.wifi.p2p.WifiP2pManager;
 import android.nfc.NfcAdapter;
 import android.nfc.Tag;
 import android.nfc.tech.IsoDep;
@@ -14,14 +17,22 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
 
+import tud.seemuh.nfcgate.network.WiFiDirectBroadcastReceiver;
+
 
 public class MainActivity extends Activity {
 
     private NfcAdapter mAdapter;
+    //private IntentFilter mIntentFilter = new IntentFilter();
     private IntentFilter mIntentFilter;
     private PendingIntent mPendingIntent;
     private IntentFilter[] mFilters;
     private String[][] mTechLists;
+
+    //WiFi Direct
+    private WifiP2pManager.Channel mChannel;
+    private WifiP2pManager mManager;
+    private BroadcastReceiver mReceiver = null;
 
     private IsoDep mSavedTag;
 
@@ -37,13 +48,23 @@ public class MainActivity extends Activity {
         return new String(hexChars);
     }
 
+    /**
+     * called at FIRST, next: onStart()
+     * @param savedInstanceState
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         mAdapter = NfcAdapter.getDefaultAdapter(this);
-        mIntentFilter = new IntentFilter("android.nfc.action.ADAPTER_STATE_CHANGED");
+        //mIntentFilter = new IntentFilter("android.nfc.action.ADAPTER_STATE_CHANGED");
+        mIntentFilter.addAction(NfcAdapter.ACTION_ADAPTER_STATE_CHANGED);
+
+        mIntentFilter.addAction(WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION);
+        mIntentFilter.addAction(WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION);
+        mIntentFilter.addAction(WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION);
+        mIntentFilter.addAction(WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION);
 
         // Create a generic PendingIntent that will be deliver to this activity.
         // The NFC stack
@@ -53,12 +74,44 @@ public class MainActivity extends Activity {
         mPendingIntent = PendingIntent.getActivity(this, 0, new Intent(this,
                 getClass()).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), 0);
 
-        // Setup an intent filter
+        // Setup an intent filter for NFC
         IntentFilter tech = new IntentFilter(NfcAdapter.ACTION_TECH_DISCOVERED);
         mFilters = new IntentFilter[] { tech, };
         mTechLists = new String[][] { new String[] { IsoDep.class.getName(),
                 NfcA.class.getName() } };
 
+        //WiFi Direct
+        mManager = (WifiP2pManager) getSystemService(Context.WIFI_P2P_SERVICE);
+        mChannel = mManager.initialize(this, getMainLooper(), null);
+    }
+
+    /**
+     * called at SECOND, next onResume()
+     * onStart(), currently not implemented
+     */
+
+    /**
+     * called at THIRD
+     */
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.i("DEBUG", "onResume(): intent: " + getIntent().getAction());
+
+        /* TODO
+        Ist NFC Aktiviert checken...
+        Utils.checkNfcEnabled(this,mAdapter);
+         */
+
+        mAdapter.enableForegroundDispatch(this, mPendingIntent, mFilters, mTechLists);
+        if (NfcAdapter.ACTION_TECH_DISCOVERED.equals(getIntent().getAction())) {
+            Log.i("NFCGATE_DEBUG", "onResume(): starting onNewIntent()...");
+            onNewIntent(getIntent());
+        }
+
+        //WiFi Direct
+        mReceiver = new WiFiDirectBroadcastReceiver(mManager, mChannel, this);
+        registerReceiver(mReceiver, mIntentFilter);
     }
 
     /**
@@ -81,26 +134,6 @@ public class MainActivity extends Activity {
             TextView view = (TextView) findViewById(R.id.hello);
             view.setText("Found Tag: " + tagId);
 
-        }
-    }
-
-    /**
-     * Will always be called?!
-     */
-    @Override
-    public void onResume() {
-        super.onResume();
-        Log.i("DEBUG", "onResume(): intent: " + getIntent().getAction());
-
-        /* TODO
-        Ist NFC Aktiviert checken...
-        Utils.checkNfcEnabled(this,mAdapter);
-         */
-
-        mAdapter.enableForegroundDispatch(this, mPendingIntent, mFilters, mTechLists);
-        if (NfcAdapter.ACTION_TECH_DISCOVERED.equals(getIntent().getAction())) {
-            Log.i("NFCGATE_DEBUG", "onResume(): starting onNewIntent()...");
-            onNewIntent(getIntent());
         }
     }
 
---
+++ app/src/main/java/tud/seemuh/nfcgate/network/WiFiDirectBroadcastReceiver.java
@@ -0,0 +1,43 @@
+package tud.seemuh.nfcgate.network;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.net.wifi.p2p.WifiP2pManager;
+
+import tud.seemuh.nfcgate.MainActivity;
+
+/**
+ * Created by daniel on 11/3/14.
+ */
+public class WiFiDirectBroadcastReceiver extends BroadcastReceiver {
+
+    private WifiP2pManager mManager;
+    private WifiP2pManager.Channel mChannel;
+    private MainActivity mActivity;
+
+    public WiFiDirectBroadcastReceiver(WifiP2pManager manager, WifiP2pManager.Channel channel,
+                                       MainActivity activity) {
+        super();
+        mManager = manager;
+        mChannel = channel;
+        mActivity = activity;
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+        String action = intent.getAction();
+
+        //Distinguish between every state from intentFilter in "onCreate()"
+        if(WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION.equals(action)) {
+
+        } else if(WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION.equals(action)) {
+
+        } else if(WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION.equals(action)) {
+
+        } else if(WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION.equals(action)) {
+
+        }
+    }
+}
