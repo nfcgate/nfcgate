047a36618899890440d0f782826bc7cf0cdce69b
Uwe Mueller <uwe@namez.de>
2015-03-01 15:46:34 +0100

    reading anticol-values from card

--- app/src/main/java/tud/seemuh/nfcgate/reader/IsoDepReaderImpl.java
+++ app/src/main/java/tud/seemuh/nfcgate/reader/IsoDepReaderImpl.java
@@ -13,22 +13,68 @@ import tud.seemuh.nfcgate.util.Utils;
  * Implements an NFCTagReader using the IsoDep technology
  *
  */
-public class IsoDepReaderImpl extends NfcAReaderImpl {
-
+public class IsoDepReaderImpl implements NFCTagReader {
     private final static String TAG = "NFC_READER_ISODEP";
     private IsoDep mAdapter = null;
 
     /**
-     * Constructor of IsoDepReader-Class, providing an NFC reader interface using the NfcA-Tech.
+     * Constructor of NfcAReader-Class, providing an NFC reader interface using the NfcA-Tech.
      *
      * @param tag: A tag using the NfcA technology.
      */
     public IsoDepReaderImpl(Tag tag) {
-        super(tag);
         Log.d(TAG, "IsoDep constructor called");
+        // Create NFC Adapter to use
         mAdapter = IsoDep.get(tag);
+        NfcA t = NfcA.get(tag);
+        Log.d(TAG, "sak: " + Utils.bytesToHex((byte)t.getSak()));
+        Log.d(TAG, "hist: " + Utils.bytesToHex(mAdapter.getHistoricalBytes()));
+        try {
+            // Connect to the NFC card
+            mAdapter.connect();
+        } catch (Exception e) {
+            //TODO
+            Log.e(TAG, "Encountered error in constructor: " + e);
+        }
     }
 
+    /**
+     * Send a raw command to the NFC chip, receiving the answer as a byte[]
+     *
+     * @param command: byte[]-representation of the command to be sent
+     * @return byte[]-representation of the answer of the NFC chip
+     */
+    public byte[] sendCmd(byte[] command) {
+        try {
+            // Transceive command and store reply
+            byte[] retval = mAdapter.transceive(command);
+
+            Log.i(TAG, "Transceived succesfully");
+
+            return retval;
+        } catch(IOException e) {
+            // TODO: Handle Exception properly
+            Log.e(TAG, "Encountered IOException in sendCmd: " + e);
+            return null;
+        } catch(Exception e) {
+            //TODO
+            Log.e(TAG, "Encountered Exception in sendCmd: " + e);
+            return null;
+        }
+    }
+
+    /**
+     * Closes the adapter, signalling that communication is over. Should be called only
+     * when no further communication with the adapter will follow, as the adapter will become
+     * unusable from this
+     */
+    public void closeConnection() {
+        try{
+            mAdapter.close();
+        } catch(IOException e) {
+            Log.e(TAG, "Encountered IOException in closeConnection: " + e);
+        }
+    }
 
     /**
      * Returns the integer representation of the protocol the used implementation is speaking.
@@ -38,6 +84,20 @@ public class IsoDepReaderImpl extends NfcAReaderImpl {
      */
     public int getProtocol() {
         return READER_ISODEP;
+    }
+
+    public boolean isConnected() { return mAdapter.isConnected(); }
+
+    public byte[] getAtqa() {
+        return NfcA.get(mAdapter.getTag()).getAtqa();
+    }
+
+    public byte getSak() {
+        return (byte)NfcA.get(mAdapter.getTag()).getSak();
+    }
+
+    public byte[] getUID() {
+        return mAdapter.getTag().getId();
     }
 
     public byte[] getHistoricalBytes() {
