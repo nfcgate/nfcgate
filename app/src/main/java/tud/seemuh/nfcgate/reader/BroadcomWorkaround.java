20f512b4406e80b1918a09833af27952c0379798
Max Maass <max@velcommuta.de>
2015-03-03 12:35:17 +0100

    Prototype code for Keepalive hack. Not yet fully implemented, but existing code works.

---
+++ app/src/main/java/tud/seemuh/nfcgate/reader/BroadcomWorkaround.java
@@ -0,0 +1,68 @@
+package tud.seemuh.nfcgate.reader;
+
+import android.nfc.Tag;
+import android.util.Log;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Objects;
+
+/**
+ * Broadcom chips have a problem with DESFire cards: They set them into a specific mode with their
+ * keepalive messages, which prevents us from sending messages using another mode. (See #33)
+ * This class is an attempt at a workaround: We will try to "fake" a keepalive loop in order to
+ * prevent Android from sending the destructive keepalive messages, as per
+ * http://www.dematte.org/2014/08/15/AndroidNFCServiceAndThinClientOneProblemAndOneHack.aspx
+ * This may or may not work, but it is at least worth a try.
+ */
+public class BroadcomWorkaround implements Runnable {
+    private String TAG = "BroadcomWorkaround";
+    private Tag mTag;
+
+    public BroadcomWorkaround(Tag tag) {
+        mTag = tag;
+    }
+
+    @Override
+    public void run() {
+        try {
+            // Get get getTagService Method
+            Method getTagServiceMethod = mTag.getClass().getDeclaredMethod("getTagService");
+            getTagServiceMethod.setAccessible(true);
+            // Retrieve tagService object (INfcTag)
+            Object tagService = getTagServiceMethod.invoke(mTag);
+            Log.d(TAG, "run: Got tagService object");
+
+            // get the getServiceHandle Method
+            Method getTagServiceHandleMethod = mTag.getClass().getDeclaredMethod("getServiceHandle");
+            getTagServiceHandleMethod.setAccessible(true);
+            // Retrieve ServiceHandle (int)
+            Object serviceHandle = getTagServiceHandleMethod.invoke(mTag);
+            Log.d(TAG, "run: got ServiceHandle object");
+
+
+            // get the getConnectedTechnology Method
+            Method getConnectedTechnologyMethod = mTag.getClass().getDeclaredMethod("getConnectedTechnology");
+            getConnectedTechnologyMethod.setAccessible(true);
+            // Retrieve ServiceHandle (int)
+            Object connectedTechnology = getConnectedTechnologyMethod.invoke(mTag);
+            Log.d(TAG, "run: got connectedTechnology object");
+
+            // Get connect method
+            Method connectMethod = tagService.getClass().getDeclaredMethod("connect", new Class[] {int.class, int.class});
+            connectMethod.setAccessible(true);
+            Log.d(TAG, "run: Got connect method object");
+            // Call connect method
+            connectMethod.invoke(tagService, serviceHandle, connectedTechnology);
+            Log.d(TAG, "run: Test successful");
+
+        } catch (NoSuchMethodException e) {
+            Log.e(TAG, "run: Method not found", e);
+        } catch (InvocationTargetException e) {
+            Log.e(TAG, "run: InvocationTargetException: ", e);
+        } catch (IllegalAccessException e) {
+            Log.e(TAG, "run: IllegalAccessException: ", e);
+        }
+
+    }
+}
