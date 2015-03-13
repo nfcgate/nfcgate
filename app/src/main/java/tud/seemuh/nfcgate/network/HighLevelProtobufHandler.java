cde6bf42c4e8ab574a0e8faf37acdcfab788b7ca
Max Maass <max@velcommuta.de>
2015-03-13 12:45:45 +0100

    Move UpdateUI to util package

--- app/src/main/java/tud/seemuh/nfcgate/network/HighLevelProtobufHandler.java
+++ app/src/main/java/tud/seemuh/nfcgate/network/HighLevelProtobufHandler.java
@@ -14,6 +14,7 @@ import tud.seemuh.nfcgate.network.c2c.C2C;
 import tud.seemuh.nfcgate.network.c2s.C2S;
 import tud.seemuh.nfcgate.network.meta.MetaMessage.Wrapper;
 import tud.seemuh.nfcgate.network.meta.MetaMessage.Wrapper.MessageCase;
+import tud.seemuh.nfcgate.util.UpdateUI;
 import tud.seemuh.nfcgate.util.Utils;
 import tud.seemuh.nfcgate.util.sink.NfcComm;
 import tud.seemuh.nfcgate.util.sink.SinkManager;
