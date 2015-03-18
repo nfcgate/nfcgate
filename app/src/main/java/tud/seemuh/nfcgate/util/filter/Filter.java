fa4b8c1d5d1636e4a16081f0516296f2a47e9bf4
Max Maass <max@velcommuta.de>
2015-03-18 14:08:04 +0100

    Stub for Filter interface and FilterManager

---
+++ app/src/main/java/tud/seemuh/nfcgate/util/filter/Filter.java
@@ -0,0 +1,8 @@
+package tud.seemuh.nfcgate.util.filter;
+
+/**
+ * Filters are used to change APDU bytes on the fly. Please check the detailed documentation in the
+ * wiki article on Filters.
+ */
+public interface Filter {
+}
---
+++ app/src/main/java/tud/seemuh/nfcgate/util/filter/FilterManager.java
@@ -0,0 +1,7 @@
+package tud.seemuh.nfcgate.util.filter;
+
+/**
+ * The FilterManager keeps track of all filters that are used on incoming and outgoing NFC traffic.
+ */
+public class FilterManager {
+}
