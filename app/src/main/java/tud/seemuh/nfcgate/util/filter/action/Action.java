9dc1f4bb1b9853b51346947d42b9c899035eeb0a
Max Maass <max@velcommuta.de>
2015-03-19 11:38:33 +0100

    Interfaces for Action and Conditionals

--- app/src/main/java/tud/seemuh/nfcgate/util/filter/action/Action.java
+++ app/src/main/java/tud/seemuh/nfcgate/util/filter/action/Action.java
@@ -5,4 +5,10 @@ package tud.seemuh.nfcgate.util.filter.action;
  * article for more information.
  */
 public interface Action {
+    /**
+     * Perform the requested action on the APDU data
+     * @param apdu NFC Data
+     * @return The modified APDU data as a byte[]
+     */
+    public byte[] performAction(byte[] apdu);
 }
--- app/src/main/java/tud/seemuh/nfcgate/util/filter/conditional/Conditional.java
+++ app/src/main/java/tud/seemuh/nfcgate/util/filter/conditional/Conditional.java
@@ -5,4 +5,10 @@ package tud.seemuh.nfcgate.util.filter.conditional;
  * Information.
  */
 public interface Conditional {
+    /**
+     * Check if the conditional matches the provided APDU.
+     * @param apdu NFC data
+     * @return True if the conditional matches, False otherwise
+     */
+    public boolean applies(byte[] apdu);
 }
