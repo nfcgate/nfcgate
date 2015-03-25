5c9548d267cb3aec418ded25cfdcb29d188ea5a8
haxxproxx <schons.tom@gmail.com>
2015-03-25 22:12:32 +0100

    fixed a very nasty bug

--- app/src/main/java/tud/seemuh/nfcgate/gui/enablenfc_dialog.java
+++ app/src/main/java/tud/seemuh/nfcgate/gui/enablenfc_dialog.java
@@ -4,7 +4,9 @@ import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.support.v4.app.DialogFragment;
 import android.view.LayoutInflater;
 
@@ -32,33 +34,31 @@ public class enablenfc_dialog extends DialogFragment {
             mListener = (NFCNoticeDialogListener) activity;
         } catch (ClassCastException e) {
             // The activity doesn't implement the interface, throw exception
-            throw new ClassCastException(activity.toString()
-                    + " must implement NoticeDialogListener");
+           // throw new ClassCastException(activity.toString()
+            //        + " must implement NoticeDialogListener");
         }
     }
 
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
-        // Build the dialog and set up the button click handlers
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
         LayoutInflater inflater = getActivity().getLayoutInflater();
-        // Inflate and set the layout for the dialog
-        // Pass null as the parent view because its going in the dialog layout
         builder.setView(inflater.inflate(R.layout.enablenfc, null))
-                .setMessage("Please enable NFC")
+                .setTitle("Please enable NFC")
+                .setMessage("Go to Settings or Dismiss?")
                 .setPositiveButton("Go to Settings", new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int id) {
-                        // Send the positive button event back to the host activity
-                        mListener.onNFCDialogPositiveClick(enablenfc_dialog.this);
+                    public void onClick(DialogInterface dialog, int which) {
+                        // go to settings
+                        Intent intent = new Intent(Settings.ACTION_NFC_SETTINGS);
+                        startActivity(intent);
                     }
                 })
                 .setNegativeButton("Dismiss", new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int id) {
-                        // Send the negative button event back to the host activity
-                        mListener.onNFCDialogNegativeClick(enablenfc_dialog.this);
+                    public void onClick(DialogInterface dialog, int which) {
+                        // do nothing
                     }
-                });
+                })
+                .setIcon(android.R.drawable.ic_dialog_alert);
         return builder.create();
     }
-
 }
