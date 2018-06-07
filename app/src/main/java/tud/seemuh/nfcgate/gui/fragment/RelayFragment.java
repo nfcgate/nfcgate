4a2b3bf333e4fca837fc9ab9adac759798bd6f3e
Steffen Klee <sklee@seemoo.tu-darmstadt.de>
2018-06-07 15:22:44 +0200

    Fixed crash on app exit when network threads report status

--- app/src/main/java/tud/seemuh/nfcgate/gui/fragment/RelayFragment.java
+++ app/src/main/java/tud/seemuh/nfcgate/gui/fragment/RelayFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
 import android.text.method.ScrollingMovementMethod;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -112,12 +113,14 @@ public class RelayFragment extends Fragment implements BaseFragment {
             super.onNetworkStatus(status);
 
             // add log entry
-            getActivity().runOnUiThread(new Runnable() {
-                @Override
-                public void run() {
-                    mLog.append("Status changed: " + status.name() + "\n");
-                }
-            });
+            final FragmentActivity activity = getActivity();
+            if (activity != null)
+                activity.runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        mLog.append("Status changed: " + status.name() + "\n");
+                    }
+                });
         }
     }
 }
