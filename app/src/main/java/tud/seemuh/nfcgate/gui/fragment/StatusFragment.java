e80f5a3d8125ef81e882bc4ef0dce318f0842190
Alexandros Roussos <alexandros.roussos@stud.tu-darmstadt.de>
2018-10-29 17:35:39 +0100

    Improved status fragment

--- app/src/main/java/tud/seemuh/nfcgate/gui/fragment/StatusFragment.java
+++ app/src/main/java/tud/seemuh/nfcgate/gui/fragment/StatusFragment.java
@@ -1,5 +1,7 @@
 package tud.seemuh.nfcgate.gui.fragment;
 
+import android.content.Context;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -7,7 +9,9 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import java.io.BufferedReader;
 import java.io.File;
@@ -17,9 +21,15 @@ import java.util.HashMap;
 import java.util.Map;
 
 import tud.seemuh.nfcgate.R;
+import tud.seemuh.nfcgate.gui.MainActivity;
 
 public class StatusFragment extends Fragment {
-    Map<String, String> mNxpMap = new HashMap<>();
+    // ui references
+    private ListView mStatus;
+    private StatusListAdapter mStatusAdapter;
+
+    // logic
+    private Map<String, String> mNxpMap = new HashMap<>();
 
     public StatusFragment() {
         mNxpMap.put("0x01","PN547C2");
@@ -35,72 +45,155 @@ public class StatusFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_status, container, false);
+        View v = inflater.inflate(R.layout.fragment_status, container, false);
+
+        // setup listview
+        mStatus = v.findViewById(R.id.status_list);
+
+        return v;
     }
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
 
-        Toast.makeText(getActivity(), detectNfcModel(), Toast.LENGTH_SHORT).show();
+        mStatusAdapter = new StatusListAdapter(getActivity(), R.layout.list_status);
+        mStatus.setAdapter(mStatusAdapter);
+
+        detect();
     }
 
-    String detectNfcModel() {
-        String chipName = "";
+    void detect() {
+        mStatusAdapter.add(detectAndroidVersion());
+        mStatusAdapter.add(detectNfcEnabled());
+        mStatusAdapter.add(detectNfcModel());
+
+        mStatusAdapter.notifyDataSetChanged();
+    }
+
+    StatusItem detectAndroidVersion() {
+        return new StatusItem("Android Version", Build.VERSION.RELEASE);
+    }
+
+    StatusItem detectNfcEnabled() {
+        return new StatusItem("NFC capability", getMainActivity().getNfc().hasNfc());
+    }
+
+    StatusItem detectNfcModel() {
+        StatusItem chipName = new StatusItem("NFC Chip", "Unknown");
 
         File confNxp = new File("/vendor/etc/libnfc-nxp.conf");
         File confNxpOld = new File("/system/etc/libnfc-nxp.conf");
         File confBrcm = new File("/vendor/etc/libnfc-brcm.conf");
         File confBrcmOld = new File("/system/etc/libnfc-brcm.conf");
 
-        if (confNxpOld.exists())
-            chipName = readNxpConf(confNxpOld, chipName);
         if (confNxp.exists())
-            chipName = readNxpConf(confNxp, chipName);
+            readNxpConf(confNxp, chipName);
+        else if(confNxpOld.exists())
+            readNxpConf(confNxpOld, chipName);
 
-        if (confBrcmOld.exists())
-            chipName = readBrcmConf(confBrcmOld, chipName);
         if (confBrcm.exists())
-            chipName = readBrcmConf(confBrcm, chipName);
+            readBrcmConf(confBrcm, chipName);
+        else if (confBrcmOld.exists())
+            readBrcmConf(confBrcmOld, chipName);
 
         return chipName;
     }
 
-    String readNxpConf(File file, String def) {
+    void readNxpConf(File file, StatusItem item) {
         // read nxp config
         try (BufferedReader br = new BufferedReader(new FileReader(file))) {
             String line;
             while ((line = br.readLine()) != null) {
                 if (line.startsWith("NXP_NFC_CHIP=")) {
                     String chipCode = line.substring(line.indexOf('=') + 1);
-                    return mNxpMap.get(chipCode);
+                    item.setValue(mNxpMap.get(chipCode));
+                    break;
                 }
             }
         }
         catch (IOException e) {
             e.printStackTrace();
         }
-        return def;
     }
 
-    String readBrcmConf(File file, String def) {
+    void readBrcmConf(File file, StatusItem item) {
         // read brcm config
         try (BufferedReader br = new BufferedReader(new FileReader(file))) {
             String line;
             while ((line = br.readLine()) != null) {
                 if (line.startsWith("FW_PRE_PATCH=")) {
                     String firmware = line.substring(line.indexOf('\"') + 1, line.lastIndexOf('\"'));
-                    return firmware.substring(17, firmware.length() - 17);
+                    item.setValue(firmware.substring(17, firmware.length() - 17));
+                    break;
                 }
                 if (line.startsWith("FW_PATCH=")) {
                     String firmware = line.substring(line.indexOf('\"') + 1, line.lastIndexOf('\"'));
-                    return firmware.substring(17, firmware.length() - 13);
+                    item.setValue(firmware.substring(17, firmware.length() - 13));
+                    break;
                 }
             }
         }
         catch (IOException e) {
             e.printStackTrace();
         }
-        return def;
+    }
+
+    protected MainActivity getMainActivity() {
+        return ((MainActivity) getActivity());
+    }
+
+
+    private class StatusItem {
+        private String mName;
+        private String mValue;
+
+        StatusItem(String name, String value) {
+            mName = name;
+            mValue = value;
+        }
+
+        StatusItem(String name, boolean yesNo) {
+            this(name, yesNo ? "yes" : "no");
+        }
+
+        public String getName() {
+            return mName;
+        }
+
+        public String getValue() {
+            return mValue;
+        }
+
+        public void setValue(String value) {
+            mValue = value;
+        }
+    }
+
+    private class StatusListAdapter extends ArrayAdapter<StatusItem> {
+        private int mResource;
+
+        StatusListAdapter(@NonNull Context context, int resource) {
+            super(context, resource);
+
+            mResource = resource;
+        }
+
+        @NonNull
+        @Override
+        public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+            View v = convertView;
+
+            if (v == null)
+                v = LayoutInflater.from(getContext()).inflate(mResource, null);
+
+            final StatusItem entry = getItem(position);
+            if (entry != null) {
+                v.<TextView>findViewById(R.id.status_name).setText(entry.getName());
+                v.<TextView>findViewById(R.id.status_value).setText(entry.getValue());
+            }
+
+            return v;
+        }
     }
 }
