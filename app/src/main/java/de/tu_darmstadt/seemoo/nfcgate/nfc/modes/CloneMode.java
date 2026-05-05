d350701cd332fbec1bb70d5d85776a23658ea73e
Alexandros Roussos <thealexroussos@gmail.com>
2026-05-05 03:34:18 +0200

    Fixed reader mode handling in clone mode

--- app/src/main/java/de/tu_darmstadt/seemoo/nfcgate/nfc/modes/CloneMode.java
+++ app/src/main/java/de/tu_darmstadt/seemoo/nfcgate/nfc/modes/CloneMode.java
@@ -8,16 +8,22 @@ public class CloneMode extends BaseMode {
     public void onEnable() {
         // reset polling and config
         mManager.resetConfig();
+        // enable reader mode
+        mManager.setReaderMode(true);
     }
 
     @Override
     public void onDisable() {
         // reset polling and config after mode ends
         mManager.resetConfig();
+        // disable reader mode
+        mManager.setReaderMode(false);
     }
 
     @Override
     public void onData(boolean isForeign, NfcComm data) {
+        // disable reader mode
+        mManager.setReaderMode(false);
         // clone tag, also disables polling
         mManager.applyData(data);
     }
